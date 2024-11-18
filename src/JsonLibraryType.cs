daee8fdc30115298eec575fea4d2656b8d4b3ca0
Jake Soenneker <jake@soenneker.com>
2024-11-18 08:34:38 -0600

    ardalis pulled out

--- src/JsonLibraryType.cs
+++ src/JsonLibraryType.cs
@@ -1,22 +1,17 @@
-﻿using Ardalis.SmartEnum;
-using Newtonsoft.Json;
-
-namespace Soenneker.Enums.JsonLibrary;
+﻿namespace Soenneker.Enums.JsonLibrary;
 
 /// <summary>
 /// An enumeration for common JSON library types
 /// </summary>
-public sealed class JsonLibraryType : SmartEnum<JsonLibraryType>
+public enum JsonLibraryType
 {
-    public static readonly JsonLibraryType SystemTextJson = new(nameof(SystemTextJson), 0);
+    /// <summary>
+    /// System.Text.Json
+    /// </summary>
+    SystemTextJson = 0,
 
     /// <summary>
     /// JSON.net
     /// </summary>
-    public static readonly JsonLibraryType Newtonsoft = new(nameof(Newtonsoft), 1);
-
-    [JsonConstructor]
-    private JsonLibraryType(string name, int value) : base(name, value)
-    {
-    }
+    Newtonsoft = 1
 }
