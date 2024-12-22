2d9265aa65809df95e0de7fd1e5786888fbe271e
Jake Soenneker <jake@soeneker.com>
2024-12-22 14:01:49 +0000

    Tests

---
+++ test/Soenneker.Enums.JsonLibrary.Tests/JsonLibraryTypeTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Enums.JsonLibrary.Tests;
+
+[Collection("Collection")]
+public class JsonLibraryTypeTests : FixturedUnitTest
+{
+    public JsonLibraryTypeTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
