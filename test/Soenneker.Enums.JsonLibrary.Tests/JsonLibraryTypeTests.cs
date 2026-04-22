0a432d67097357fb09b543c08f280d0cabe03ea6
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:46 -0500

    TUnit conversion

--- test/Soenneker.Enums.JsonLibrary.Tests/JsonLibraryTypeTests.cs
+++ test/Soenneker.Enums.JsonLibrary.Tests/JsonLibraryTypeTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Enums.JsonLibrary.Tests;
 
-[Collection("Collection")]
-public class JsonLibraryTypeTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class JsonLibraryTypeTests : HostedUnitTest
 {
-    public JsonLibraryTypeTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public JsonLibraryTypeTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
